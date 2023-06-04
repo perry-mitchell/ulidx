64b17a3e7c299b04997b6ac0fcb4b0d6518381da
Perry Mitchell <perry@perrymitchell.net>
2023-06-04 21:10:33 +0300

    Build all modes using rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,45 @@
 import { builtinModules } from "node:module";
 import typescript from "@rollup/plugin-typescript";
 import resolve from "@rollup/plugin-node-resolve";
+import alias from "@rollup/plugin-alias";
 import pkg from "./package.json" assert { type:"json" };
 
 const EXTENSIONS = [".js", ".ts"];
+const MODE = process.env.MODE ? process.env.MODE : "node";
+
+// const externals = MODE === "node" ? [
+//     ...builtinModules,
+//     ...(pkg.dependencies ? Object.keys(pkg.dependencies) : [])
+// ] : [...builtinModules];
+
+const plugins = [
+    typescript({
+        tsconfig: "tsconfig.json"
+    }),
+    resolve({ extensions: EXTENSIONS })
+];
+if (MODE === "browser") {
+    plugins.unshift(alias({
+        entries: [
+            { find: "node:crypto", replacement: "./stub.js" }
+        ]
+    }));
+}
 
 export default {
-    external: [
-        ...builtinModules,
-        ...(pkg.dependencies ? Object.keys(pkg.dependencies) : [])
-    ],
+    external: [...builtinModules],
     input: "source/index.ts",
     output: [
         {
-            dir: "dist/cjs",
+            dir: `dist/${MODE}`,
             format: "cjs",
             entryFileNames: "[name].cjs"
         },
         {
-            dir: "dist/esm",
-            format: "esm"
+            dir: `dist/${MODE}`,
+            format: "esm",
+            entryFileNames: "[name].js"
         }
     ],
-    plugins: [typescript({
-        tsconfig: "tsconfig.json"
-    }), resolve({ extensions: EXTENSIONS })]
+    plugins
 };
