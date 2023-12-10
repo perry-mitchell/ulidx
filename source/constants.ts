780095f44a7a6fbbe3daea7de3534cea46c7f8cb
Jesús Valero <jvalerodev@gmail.com>
2023-12-10 14:29:19 -0400

    feat: convert ULID to UUID and vice versa

--- source/constants.ts
+++ source/constants.ts
@@ -1,2 +1,6 @@
 export const MAX_ULID = "7ZZZZZZZZZZZZZZZZZZZZZZZZZ";
 export const MIN_ULID = "00000000000000000000000000";
+
+export const ULID_REGEX = /^[0-7][0-9a-hjkmnp-tv-zA-HJKMNP-TV-Z]{25}$/;
+export const UUID_REGEX = /^[0-9a-fA-F]{8}-(?:[0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12}$/;
+export const B32_CHARACTERS = "0123456789ABCDEFGHJKMNPQRSTVWXYZ";
